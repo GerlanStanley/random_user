5ebc999a1b2de05fe0cf95fdd89635e60ba328e6
Gerlan Stanley <gerlangerlan@hotmail.com>
2023-05-02 22:11:23 -0300

    Injetando dependências do core

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,11 +1,17 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import 'core/helpers/http/http.dart';
 import 'modules/splash/presenter/pages/splash_page.dart';
 import 'modules/user/user_module.dart';
 
 class AppModule extends Module {
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds => [
+        Bind.lazySingleton<Dio>((i) => Dio()),
+        Bind.lazySingleton<Interceptor>((i) => CustomInterceptor()),
+        Bind.lazySingleton<HttpHelper>((i) => HttpHelperImpl(i(), i())),
+      ];
 
   @override
   List<ModularRoute> get routes => [
