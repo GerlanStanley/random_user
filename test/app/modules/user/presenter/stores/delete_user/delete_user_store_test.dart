b7420dc534766903c82d2691ee2787e80884e452
Gerlan Stanley <gerlangerlan@hotmail.com>
2023-05-05 12:36:10 -0300

    Teste unitários para os stores

---
+++ test/app/modules/user/presenter/stores/delete_user/delete_user_store_test.dart
@@ -0,0 +1,118 @@
+import 'package:dartz/dartz.dart';
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mobx/mobx.dart' as mobx;
+import 'package:mocktail/mocktail.dart';
+
+import 'package:user/app/core/failures/failures.dart';
+import 'package:user/app/modules/user/domain/entities/location_entity.dart';
+import 'package:user/app/modules/user/domain/entities/name_entity.dart';
+import 'package:user/app/modules/user/domain/entities/picture_entity.dart';
+import 'package:user/app/modules/user/domain/entities/user_entity.dart';
+import 'package:user/app/modules/user/domain/use_cases/delete_user_use_case.dart';
+import 'package:user/app/modules/user/presenter/stores/delete_user/delete_user_state.dart';
+import 'package:user/app/modules/user/presenter/stores/delete_user/delete_user_store.dart';
+
+class MockDeleteUserUseCase extends Mock implements DeleteUserUseCase {}
+
+abstract class Callable<T> {
+  void call([T? arg]) {}
+}
+
+class MockCallable<T> extends Mock implements Callable<T> {}
+
+void main() {
+  late MockDeleteUserUseCase useCase;
+  late DeleteUserStore store;
+  late UserEntity user;
+
+  setUp(() {
+    useCase = MockDeleteUserUseCase();
+    store = DeleteUserStore(useCase);
+
+    user = UserEntity(
+      uuid: faker.randomGenerator.integer(999).toString(),
+      age: faker.randomGenerator.integer(80),
+      username: faker.person.name(),
+      birthdate: faker.date.dateTime(),
+      cell: faker.phoneNumber.us(),
+      phone: faker.phoneNumber.us(),
+      email: faker.internet.email(),
+      gender: 'female',
+      nationality: 'US',
+      name: NameEntity(
+        title: 'Mr',
+        first: faker.person.name(),
+        last: faker.person.name(),
+      ),
+      location: LocationEntity(
+        city: faker.address.city(),
+        country: faker.address.country(),
+        state: faker.address.state(),
+        streetName: faker.address.streetName(),
+        streetNumber: faker.randomGenerator.integer(9999),
+        postcode: faker.randomGenerator.integer(9999).toString(),
+        latitude: faker.randomGenerator.integer(9999).toString(),
+        longitude: faker.randomGenerator.integer(9999).toString(),
+        timezoneOffset: '+3.0',
+      ),
+      picture: PictureEntity(
+        thumbnail: '',
+        medium: '',
+        large: '',
+      ),
+    );
+
+    registerFallbackValue(user);
+  });
+
+  test(
+    "Quando o useCase retornar um sucesso o "
+    "bloc deve iniciar carregando e terminar com sucesso",
+    () async {
+      final statusChanged = MockCallable<DeleteUserState>();
+
+      when(() => useCase.call(user: any(named: 'user'))).thenAnswer(
+        (_) async => const Right(true),
+      );
+
+      mobx.reaction<DeleteUserState>(
+        (_) => store.state,
+        (newValue) => statusChanged(newValue),
+      );
+
+      await store.delete(user: user);
+
+      verifyInOrder([
+        () => statusChanged(LoadingDeleteUserState()),
+        () => statusChanged(SuccessDeleteUserState()),
+      ]);
+    },
+  );
+
+  test(
+    "Quando o useCase retornar uma falha o "
+    "bloc deve iniciar carregando e terminar com falha",
+    () async {
+      final statusChanged = MockCallable<DeleteUserState>();
+
+      when(() => useCase.call(user: any(named: 'user'))).thenAnswer(
+        (_) async => Left<Failure, bool>(
+          Failure(message: ""),
+        ),
+      );
+
+      mobx.reaction<DeleteUserState>(
+        (_) => store.state,
+        (newValue) => statusChanged(newValue),
+      );
+
+      await store.delete(user: user);
+
+      verifyInOrder([
+        () => statusChanged(LoadingDeleteUserState()),
+        () => statusChanged(const FailureDeleteUserState(error: '')),
+      ]);
+    },
+  );
+}
